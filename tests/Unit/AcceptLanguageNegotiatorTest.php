8de9ca2974c42432f804d0159d7611d45ca0f2f8
Dominik Zogg <dominik.zogg@hotmail.com>
2024-02-04 20:10:11 +0100

    Revert "cs-fix"
    This reverts commit 64dd411c51cc8cc9fc81f42b7f059e7c2d75390c.

--- tests/Unit/AcceptLanguageNegotiatorTest.php
+++ tests/Unit/AcceptLanguageNegotiatorTest.php
@@ -63,7 +63,7 @@ final class AcceptLanguageNegotiatorTest extends TestCase
         self::assertEquals($expectedAcceptLanguage, $negotiator->negotiate($request));
     }
 
-    public static function provideNegotiateCases(): iterable
+    public function provideNegotiateCases(): iterable
     {
         return [
             [
